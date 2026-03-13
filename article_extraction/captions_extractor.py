d9b30fa0b4c783388751ceacee25c4022a57b0df
yiduo <yiduo.fang25@imperial.ac.uk>
2026-03-13 15:09:56 +0000

    add captions, figure, tables feature

---
+++ article_extraction/captions_extractor.py
@@ -0,0 +1,155 @@
+from bs4 import BeautifulSoup
+import re
+
+def rsc_captions(soup):
+    '''RSC, MDPI'''
+    captions = []
+    for cap in soup.find_all(["td"], class_=lambda c: c and "image_title" in c.lower()):
+        captions.append(cap.get_text(strip=True))
+    for cap in soup.find_all(["div", "p"], class_=lambda c: c and "caption" in c.lower()):
+        captions.append(cap.get_text(strip=True))
+    captions = list(dict.fromkeys(captions))
+    results = structure_figure_captions(captions)
+    return results
+
+def tandf_captions(soup):
+    captions = []
+    for cap in soup.find_all(["div", "p"], class_=lambda c: c and "caption" in c.lower()):
+        captions.append(cap.get_text(strip=True))
+    captions = list(dict.fromkeys(captions))
+    results = structure_figure_captions(captions)
+    return results
+
+def acs_captions(soup):
+    '''ACS, Frontiers'''
+    captions = []
+    for fig in soup.find_all('figure', id = True):
+        id = fig.get('id')
+        if id.startswith('fig'):
+            cap = fig.find('p').get_text(strip=True)
+            captions.append(cap)
+        elif id.startswith('sch'):
+            scheme_label = fig.find("h2", class_="fig-label").get_text(strip=True)
+            texts = []
+            for p in fig.find_all("p"):
+                for span in p.find_all("span", class_="fn-label"):
+                    span.decompose()     # remove Scheme a/b/c
+                txt = p.get_text(strip=True)
+                if txt:
+                    texts.append(txt)
+            cap = scheme_label + " " + " ".join(texts)
+            captions.append(cap)
+    for tab in soup.find_all('div', class_='NLM_caption'):
+        cap = tab.get_text(strip=True)
+        captions.append(cap)
+    captions = list(dict.fromkeys(captions)) 
+    captions = [c for c in captions if c.lstrip().lower().startswith(("figure", "table"))]
+    results = structure_figure_captions(captions)
+    return results
+
+def nature_captions(soup):
+    captions = []
+    for cap in soup.find_all("figcaption"):
+        figure = cap.parent
+        subcap = figure.find("div", {"data-test": "bottom-caption"})
+        if subcap:
+            p_tag = subcap.find("p")
+            p_text = p_tag.get_text(separator=" ", strip=True) if p_tag else ""
+            caption = cap.get_text(strip=True) + " " + p_text
+            captions.append(caption)
+        else:
+            captions.append(cap.get_text(strip=True))
+    for cap in soup.find_all("caption"):
+        captions.append(cap.get_text(strip=True))
+    captions = list(dict.fromkeys(captions))
+    results = structure_figure_captions(captions)
+
+    return results
+
+def science_captions(soup):
+    captions = []
+    for cap in soup.find_all("figcaption"):
+        text = cap.get_text(strip=True)
+        text = text.replace('\xa0', '')
+        captions.append(text)
+    captions = list(dict.fromkeys(captions))
+    results = structure_figure_captions(captions)
+
+    return results
+
+def wiley_captions(soup):
+    captions = []
+    for cap in soup.find_all(["header"], class_=lambda c: c and "caption" in c.lower()):
+        captions.append(cap.get_text(strip=True))
+    for cap in soup.find_all("figcaption"):
+        text = cap.get_text(strip=True).replace('Open in figure viewerPowerPoint', '')
+        captions.append(text)
+    captions = list(dict.fromkeys(captions))
+    results = structure_figure_captions(captions)
+    return results
+
+def springer_captions(soup):
+    captions = []
+    for cap in soup.find_all(["div"], class_=lambda c: c and "figure" in c.lower()):
+        captions.append(cap.get_text(strip=True))
+    for cap in soup.find_all("figcaption"):
+        captions.append(cap.get_text(strip=True))
+    captions = [c for c in captions if c.lstrip().lower().startswith(("fig.", "table"))] #remove strings that do not start with "Fig." or "Table"
+    captions = [c for c in captions if len(c) >= 8]
+    captions = list(dict.fromkeys(captions))
+    results = structure_figure_captions(captions)
+    return results
+
+def elsevier_captions(soup):
+    '''Elsevier, Springer'''
+    valid_label = re.compile(r'^(Fig\.?|Figure|Table|Scheme)\b', re.IGNORECASE)
+    captions = []
+    for label in soup.find_all("ce:label"):
+        figure_number = label.get_text(strip=True)
+        if not valid_label.match(figure_number):
+            continue
+        caption_tag = label.find_next("ce:caption")
+        if caption_tag:
+            cap = figure_number + " " + caption_tag.get_text(strip=True)
+            captions.append(cap)
+
+    results = structure_figure_captions(captions)
+    return results
+
+def concat_strings(captions):
+    tables_caps = []
+    for c in captions:
+        if c.lower().startswith('table'):
+            tables_caps.append(c.strip())
+
+    fig_caps = []
+    for c in captions:
+        if c not in tables_caps:
+            fig_caps.append(c.strip())
+
+    return tables_caps, fig_caps
+
+def structure_figure_captions(captions):
+    tables_caps, figures_caps = concat_strings(captions)
+    results = []
+    if figures_caps:
+        result_figure = {
+            "name": "Figures",
+            "type": "captions",
+            "content": figures_caps
+        }
+        results.append(result_figure)
+
+    if tables_caps:
+        result_tables = {
+            "name": "Tables",
+            "content": tables_caps
+        }
+        results.append(result_tables)
+
+    return results
+
+
+
+
+
---
+++ article_extraction/tables_extractor.py
@@ -0,0 +1,130 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import json
+import re
+import captions_extractor
+
+def nature_table(soup)->list[dict]:
+    tables = soup.find_all('a', attrs={'data-test': 'table-link'})
+    table_dicts = []
+    for table in tables:
+        table_dict = {}
+        label = table['aria-label']
+        table_dict['label'] = label
+        table_link = table['href']
+        table_link = urljoin('https://www.nature.com', table_link)
+        html = requests.get(table_link).text
+        soup = BeautifulSoup(html, "html.parser")
+        table = soup.find('table')
+        text = str(table)
+        table_dict['content'] = text
+        table_dicts.append(table_dict)
+    return table_dicts
+
+def springer_table(soup)->list[dict]:
+    tables = soup.find_all('a', attrs={'data-test': 'table-link'})
+    table_dicts = []
+    for table in tables:
+        table_dict = {}
+        label = table['aria-label']
+        table_dict['label'] = label
+        table_link = table['href']
+        table_link = urljoin('https://link.springer.com/', table_link)
+        html = requests.get(table_link).text
+        soup = BeautifulSoup(html, "html.parser")
+        table = soup.find('table')
+        text = str(table)
+        table_dict['content'] = text
+        table_dicts.append(table_dict)
+    return table_dicts
+
+def rsc_table(soup):
+    tables = soup.find_all('table', class_='tgroup')
+    table_list = []
+    for table in tables:
+        table_dict = {}
+        label = table.find_parent('div', class_='rtable__wrapper') \
+                    .find_previous_sibling('div', class_='table_caption').find('b').text
+        table_dict['label'] = label
+        text = str(table)
+        table_dict['content'] = text
+        table_list.append(table_dict)
+
+    return table_list
+
+def acs_table(soup):
+    tables = soup.find_all('table', class_='table')
+    table_list = []
+    for table in tables:
+        table_dict = {}
+        label = table.find_parent('div', class_='NLM_table-wrap').get('id')
+        table_dict['label'] = label
+        text = str(table)
+        table_dict['content'] = text
+        table_list.append(table_dict)
+
+    return table_list
+
+def science_table(soup):
+    tables = soup.find_all('table')
+    table_dicts = []
+    for table in tables:
+        table_dict = {}
+        label = table.find_parent('figure', class_='table').get('id')
+        table_dict['label'] = label
+        table_dict['content'] = str(table)
+        table_dicts.append(table_dict)
+    return table_dicts
+
+def mdpi_table(soup):
+    divs = soup.find_all('div', class_='html-table_show')
+    table_dicts = []
+    for div in divs:
+        table_dict = {}
+        label = div.find('b').get_text()
+        table_dict['label'] = label
+        table_dict['content'] = str(div.find('table'))
+        table_dicts.append(table_dict)
+    return table_dicts
+
+def wiley_table(soup):
+    tables = soup.find_all('table', class_='article-section__table')
+    table_dicts = []
+    for table in tables:
+        table_dict = {}
+        label = table.find_parent().find_previous_sibling('header').find('span').get_text().strip()
+        table_dict['label'] = label
+        table_dict['content'] = str(table)
+        table_dicts.append(table_dict)
+    return table_dicts
+
+def tandf_table(soup):
+    script = soup.find("script", string=lambda s: s and "tandf.tfviewerdata" in s)
+    data = json.loads(re.search(r"tandf\.tfviewerdata\s*=\s*({.*});", script.string, re.S).group(1))
+    tables = data["tables"]
+    table_dicts = []
+    for table in tables:
+        table_dict = {}
+        tbl_soup = BeautifulSoup(table["content"], "html.parser")
+        table_element = tbl_soup.find("table")
+        table_dict['content'] = str(table_element)
+        caption = tbl_soup.find("caption")
+        label_span = caption.find("span", class_="captionLabel") if caption else None
+        label_text = label_span.get_text(" ", strip=True) if label_span else caption.get_text(" ", strip=True).split(".")[0] + "."
+        table_dict['label'] = label_text
+        table_dicts.append(table_dict)
+    return table_dicts
+
+def elsevier_table(soup):
+    tables = soup.find_all('tgroup')
+    table_dicts = []
+    for table in tables:
+        table_dict = {}
+        label = table.find_previous_sibling('ce:label').get_text()
+        table_dict['label'] = label
+        text = str(table)
+        content = re.sub(r'\s+xmlns="[^"]*"', '', text)
+        table_dict['content'] = content
+        table_dicts.append(table_dict)
+    return table_dicts
